00d4d5a9a25ab29568abdd1773386e73f9c1c053
jetharleycambe <jetharley.cambe@gmail.com>
2026-05-15 13:59:21 +0800

    Add files via upload

--- api/db.php
+++ api/db.php
@@ -11,10 +11,15 @@ $port = 28987;
 $conn = mysqli_init();
 
 // 3. Point to the SSL certificate you downloaded
-$ca_path = realpath(__DIR__ . '/../ca.pem');
+// This tells PHP to look in the main folder (root) for the pem file
+$ca_path = $_SERVER['DOCUMENT_ROOT'] . '/ca.pem';
+
+// If that doesn't exist, it falls back to the relative path
+if (!file_exists($ca_path)) {
+    $ca_path = realpath(__DIR__ . '/../ca.pem');
+}
 
 mysqli_ssl_set($conn, NULL, NULL, $ca_path, NULL, NULL);
-
 // 4. Establish the connection
 $real_connect = mysqli_real_connect(
     $conn, 
